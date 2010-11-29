d584fe7174986a18edd33aaf28613ac7ee2d5b24
GRP9-Doris <Birgit.Eschinger@gmx.at>
2010-11-29 11:15:49 +0100

    Commit Doris 11:15

--- src/at/jku/ce/DomainObject.java
+++ src/at/jku/ce/DomainObject.java
@@ -29,4 +29,12 @@ public class DomainObject {
 	public void setComment(String comment) { 
 		this.comment = comment; 
 	}
+	
+	public String getName() { 
+		return name; 
+	} 
+	
+	public void setName(String name) { 
+		this.name = name; 
+	}
 }
